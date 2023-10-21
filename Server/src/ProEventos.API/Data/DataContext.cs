1031863bfc8d298a3ad861c918a86a15ffb38446
RenatoPereirals <renatopreirals@gmail.com>
2023-10-21 00:49:09 -0300

    Instalação do EF-Core, EF-PostegreSQL e EF-PostegreSQL.Design

---
+++ Server/src/ProEventos.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProEventos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        
+    }
+}
--- Server/src/ProEventos.API/Startup.cs
+++ Server/src/ProEventos.API/Startup.cs
@@ -1,16 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using ProEventos.API.Data;
 
 namespace ProEventos.API
 {
@@ -31,6 +21,11 @@ namespace ProEventos.API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
+            });
+            services.AddDbContext<DataContext>(options =>
+            {
+                var connectionString = Configuration.GetConnectionString("PostgreSQLConnection") ?? "DefaultConnection";
+                options.UseNpgsql(connectionString);
             });
         }
 
