32ef63f396f4b6e17a24e9fc5ab46b002684482c
RenatoPereirals <renatopreirals@gmail.com>
2023-10-24 11:26:12 -0300

    Criação dos domínios, ajustes gerais e criação da interface de persistência

---
+++ Server/src/Persistence/IProeventosPersistence.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Domain;
+
+namespace Persistence
+{
+    public interface IProeventosPersistence
+    {
+        //Geral
+        void Add<T>(T entity) where T: class;
+        void Update<T>(T entity) where T: class;
+        void Delete<T>(T entity) where T: class;
+        void DeleteRange<T>(T entity) where T: class;
+        task<bool> SaveChangesAsync();
+
+        //Eventos
+        Task<Event[]> GetAllEventsByThemeAsync(string Theme, bool includeSpeakers);
+        Task<Event[]> GetAllEventsAsync(bool includeSpeakers);
+        Task<Event> GetEventByIdAsync(int EventId, bool includeSpeakers);
+
+        //Speakers
+        Task<Speaker[]> GetAllSpeakersByNameAsync(string Name, bool includeEvents);
+        Task<Speaker[]> GetAllSpeakersAsync(bool includeEvents);
+        Task<Speaker> GetSpeakerByIdAsync(int SpeakerId, bool includeEvents);
+    }
+}
